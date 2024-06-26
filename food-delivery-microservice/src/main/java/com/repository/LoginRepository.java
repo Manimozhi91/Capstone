faa596e629dcda32e3235834e123287b3b6437cf
Manimozhi91 <105507953+Manimozhi91@users.noreply.github.com>
2024-06-26 10:42:20 +0530

    Delete food-delivery-microservice directory

--- food-delivery-microservice/src/main/java/com/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Login;
-
-public interface LoginRepository extends JpaRepository<Login, String>{
-
-}
